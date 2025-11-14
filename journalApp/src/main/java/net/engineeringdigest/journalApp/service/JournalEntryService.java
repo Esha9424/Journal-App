cd0893ee14bf91b038cba80674b4669027e8b5f2
Esha <esha2103rita@gmail.com>
2025-11-14 16:44:25 +0530

    service class bean bn gyi now controller me service ka obj null nhi hoga

--- journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/service/JournalEntryService.java
@@ -3,7 +3,9 @@ package net.engineeringdigest.journalApp.service;
 import net.engineeringdigest.journalApp.entity.JournalEntry;
 import net.engineeringdigest.journalApp.repository.JournalEntryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class JournalEntryService {
     @Autowired
     private JournalEntryRepository journalEntryRepository;
