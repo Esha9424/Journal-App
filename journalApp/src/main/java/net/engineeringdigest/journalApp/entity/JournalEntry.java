76d3f689bc460d0d36835d61a97fead12a2367bf
Esha <esha2103rita@gmail.com>
2025-11-15 20:31:29 +0530

    lombok installed and annotation is added to make lambok work

--- journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -1,5 +1,6 @@
 package net.engineeringdigest.journalApp.entity;
 
+import lombok.Data;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -8,6 +9,7 @@ import java.time.LocalDateTime;
 import java.util.Date;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
@@ -15,36 +17,7 @@ public class JournalEntry {
     private String tittle;
     private String content;
     private LocalDateTime date;
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
 
 
 
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-    public String getTittle() {
-        return tittle;
-    }
-
-    public void setTittle(String tittle) {
-        this.tittle = tittle;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
